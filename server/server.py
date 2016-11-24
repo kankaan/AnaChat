8bb7694ebf3f766e10e91e3714a4c4e10dd9c79d
Antti Kankaanranta <antti.kankaanranta@gmail.com>
2016-11-24 22:40:31 +0200

    added database SQLite and couple of tables

--- server/server.py
+++ server/server.py
@@ -1,19 +1,46 @@
 from flask import Flask,request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_sqlalchemy import SQLAlchemy
+#import models
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///anaChat.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
-class user:
-	# class for user information
-	def __init__(self):
-		self.name = ""
-		self.pwHash = ""
-		self.chats = []
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    #email = db.Column(db.String(120), unique=True)
+    def __init__(self, username):
+        self.username = username
+        self.email = ""
+    def __repr__(self):
+        return '<User %r>' % self.username
 
+
+db.create_all()
+parser = reqparse.RequestParser()
+parser.add_argument('message')
+parser.add_argument('userName')
+
+# API class for user
+# Post will create new user
 class userApi(Resource):
 	def get(self):
 		return "moi"
+	#def post(self, userFuntion):
+	def post(self):
+		args = parser.parse_args()
+		print(args['userName'])
+		if (User.query.filter_by(username=args['userName']).first() == None):
+			newUser = User(args['userName'])
+			db.session.add(newUser)
+			db.session.commit()
+			return "user added"
+		else:
+			return "user was there already"
 
 class chat:
 	# chat object
@@ -28,8 +55,21 @@ class chat:
 	def readMessages(self):
 		return self.messages
 
+chatY = {}
+# API for chat:
+# Chat handles messages
+class chatAPI(Resource):
+	def get(self):
+		return chaty
+	def post(self,chatName):
+		args = parser.parse_args()
+		print("cat ",chatName)
+		print("post: ",args['message'])
+		chatY[chatName] = args['message']
+		return {chatName:chatY[chatName]}
 
-api.add_resource(userApi,'/')
+api.add_resource(userApi,'/user/')
+api.add_resource(chatAPI, '/chat/<string:chatName>')
 
 if __name__ == '__main__':
 	app.run(debug=True)
