4a4755bcbfbc5c1d12fb0c9c5e54efb480ba384f
Antti Kankaanranta <antti.kankaanranta@gmail.com>
2016-11-28 23:08:17 +0200

    login decorator created for the server.

--- server/server.py
+++ server/server.py
@@ -1,40 +1,103 @@
-from flask import Flask,request
+import os
+from flask import Flask,request, g,Response
 from flask_restful import Resource, Api, reqparse
 from flask_sqlalchemy import SQLAlchemy
 from passlib.apps import custom_app_context as pwd_context
-#import models
+from itsdangerous import (JSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
+import json
 
+#Let's crete the app
 app = Flask(__name__)
+app.debug = True
 api = Api(app)
+#configuration should be moved to different file
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///anaChat.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = os.urandom(24) 
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
+'''
+class Tokens(db.Model):
+	__table__ = 'Tokens'
+	id = db.Column(db.Integer, primary_key=True)
+	token = db.Column(db.String)
+	def __init__(self,token):
+		self.token = token
+'''
+authTokens = []
 
+#user class for storing user information
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     password_hash = db.Column(db.String(128))
-    def __init__(self, username, pw):
+    def __init__(self, username, password):
         self.username = username
         self.email = ""
         self.password_hash = pwd_context.encrypt(password)
-    def __repr__(self):
-        return '<User %r>' % self.username
+    
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
+	
+    def generate_auth_token(self, expiration = 1000):
+        s = Serializer(app.config['SECRET_KEY'])
+        return s.dumps({ 'id': self.id })
+	
+    def generate_auth_headers(self,expiration =600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
+        headerToken = s.dumps({'id':self.id})
+        authTokens.push(headerToken)
+        #header = s.make_header({'token':headerToken})
+        return header
+        #return Response(headers=header)
+        #return json.dumps(header)
 
+    @staticmethod
+    def verify_auth_token( token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            print("saatu token: ",token)
+            data = s.loads(token)
+        except SignatureExpired:
+            print("signaure expired")
+            return None # valid token, but expired
+        except BadSignature:
+            print('badsigna')
+            return None # invalid token
+        user = User.query.get(data['id'])
+        return user
 
 db.create_all()
+
+# reqparser from flask_restful
 parser = reqparse.RequestParser()
 parser.add_argument('message')
-parser.add_argument('userName')
-parser.add_argument('pWord')
+parser.add_argument('username')
+parser.add_argument('password')
+parser.add_argument('token',location='headers')
+
+def loginDecorate(auth):
+	def token(*args, **kwargs):
+		#check the users token here and return the function
+		args = parser.parse_args()
+		print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+		if (args['token'] == None):
+			return "not authenticated"
+		print("token ", args['token'])
+		print("user: ",User.verify_auth_token(args['token']))
+		if (User.verify_auth_token(args['token']) == None):
+			return "false"
+		return auth
+	return token
 
 # API class for user
 # Post will create new user
 class userApi(Resource):
+	@loginDecorate
 	def get(self):
-		return "moi"
+		return {'resp':"True user"}
 	#def post(self, userFuntion):
 	def post(self):
 		args = parser.parse_args()
@@ -47,6 +110,8 @@ class userApi(Resource):
 			return "user added"
 		else:
 			return "user was there already"
+
+
 
 class chat:
 	# chat object
@@ -74,8 +139,33 @@ class chatAPI(Resource):
 		chatY[chatName] = args['message']
 		return {chatName:chatY[chatName]}
 
-api.add_resource(userApi,'/user/')
-api.add_resource(chatAPI, '/chat/<string:chatName>')
+class login(Resource):
+	def post(self):
+		args = parser.parse_args()
+		print("kayttaja ",args['username'])
+		print("salasana ",args['password'])
+		user = User.query.filter_by(username = args['username']).first()
+		if (not user or not user.verify_password(args['password'])):
+			return False
+		token = user.generate_auth_token()	
+		authTokens.append(token)
+		return {"token":token}
+
+	def get(self):
+		token = g.user.generate_auth_token(600)
+		return jsonify({'token': token.decode('ascii'), 'duration': 600})	
+
+class logout(Resource):
+	def post(self):
+		args = parser.parse_args()
+		token = args['token']
+		if token in authTokens: authTokens.remove(token)
+		return "Logout"
+
+api.add_resource(login, '/api/login/')
+api.add_resource(logout, '/api/logout/')
+api.add_resource(userApi,'/api/user/')
+api.add_resource(chatAPI, '/api/chat/<string:chatName>')
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- server/usertest.py
+++ server/usertest.py
@@ -1,27 +1,34 @@
 import requests
+import os
 import unittest
 import tempfile
 import server
-#import server
-# configurate common url for tests
-url = "http://127.0.0.1:5000/"
-urlUser = ""
 
 class UserTest(unittest.TestCase):
 	def setUp(self):
-		self.db_fd, app.config['SQLALCHEMY_DATABASE_URI'] = tempfile.mkstemp()
+		self.db_fd, server.app.config['SQLALCHEMY_DATABASE_URI'] = \
+			tempfile.mkstemp()
 		server.app.config['TESTING'] = True
 		self.app = server.app.test_client()
 		
 	def tearDown(self):
 		os.close(self.db_fd)
-		os.unlink( app.config['SQLALCHEMY_DATABASE_URI'])
+		os.unlink( server.app.config['SQLALCHEMY_DATABASE_URI'])
 
-	def addNewUser(self):
+	def addNewUser(self,user,pw):
 		return self.app.post("/user", data= dict(
-			userName='newUser',
-			pWord='password'))
+			userName=user,
+			pWord=pw))
 
+	def test_moi(self):
+		rv = self.app.get("/user")
+		with  open("debug.txt","w") as t:
+			t.write(i)
+		assert "moi" in "moi"
+
+	def test_AddNewUser(self):
+		rv = self.addNewUser('TimoTestaaja',"cryptedPW")
+		#assert b"user added" in rv.data
 
 if __name__ == "__main__":
 	unittest.main()
